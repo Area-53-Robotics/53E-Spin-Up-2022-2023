25f8f70a1f8ae2ce4f3d8bbc9e50b7c096e28559
AfootHat9199896 <alananthonymorales@gmail.com>
2022-11-01 17:16:16 -0700

    most recent updated drive code with all functions

--- include/devices.h
+++ include/devices.h
@@ -1,3 +1,4 @@
+
 #include "api.h"
 #include "pros/adi.hpp"
 #include "pros/motors.h"
@@ -6,21 +7,20 @@ using namespace pros;
 
 inline Controller controller(E_CONTROLLER_MASTER);
 
-inline Motor left1(9, E_MOTOR_GEAR_BLUE, true);
-inline Motor left2(10, E_MOTOR_GEAR_BLUE, true);
-inline Motor left3(8, E_MOTOR_GEAR_BLUE, true);
+inline Motor left1(16,E_MOTOR_GEAR_BLUE,true);
+inline Motor left2(17,E_MOTOR_GEAR_BLUE,true);
+inline Motor left3(18,E_MOTOR_GEAR_BLUE,true);
 
-inline Motor right1(20, E_MOTOR_GEAR_BLUE);
-inline Motor right2(19, E_MOTOR_GEAR_BLUE);
-inline Motor right3(18, E_MOTOR_GEAR_BLUE);
+inline Motor right1(11,E_MOTOR_GEAR_BLUE);
+inline Motor right2(12,E_MOTOR_GEAR_BLUE);
+inline Motor right3(13,E_MOTOR_GEAR_BLUE);
 
-inline Motor rollerMotor(7, E_MOTOR_GEAR_GREEN);
+inline Motor rollerMotor(20,E_MOTOR_GEAR_GREEN);
 
-inline Motor launcherMotor(5, E_MOTOR_GEAR_BLUE, true);
+inline Motor launcherMotor(11,E_MOTOR_GEAR_BLUE, true);
 
+inline pros::Motor_Group leftMotors ({left1, left2, left3});
+inline pros::Motor_Group rightMotors ({right1, right2, right3});
 
-inline pros::Motor_Group leftMotors({left1, left2, left3});
-inline pros::Motor_Group rightMotors({right1, right2, right3});
+inline ADIEncoder encoder (2,1);
 
-inline ADIEncoder encoder(7, 8);
-inline ADIEncoder launcherEncoder(1, 2);
