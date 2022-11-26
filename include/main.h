0e80170981b6831a82d4b21704d77bc3d94fbf4a
BattleCh1cken <BattleCh1cken@Larkov.de>
2022-11-25 22:48:23 -0500

    feat: sylib

--- include/main.h
+++ include/main.h
@@ -35,14 +35,13 @@
 #define PROS_USE_LITERALS
 
 #include "api.h"
+#include "sylib/sylib.hpp"
 
 /**
  * You should add more #includes here
  */
-
 #include "devices.h"
 #include "helpers.h"
-
 //#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
 
@@ -54,7 +53,7 @@
  * concurrently! The okapi namespace will export all symbols inside the pros
  * namespace.
  */
-using namespace pros;
+ using namespace pros;
 // using namespace pros::literals;
 // using namespace okapi;
 
@@ -66,23 +65,20 @@ using namespace pros;
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 void autonomous(void);
 void initialize(void);
 void disabled(void);
 void competition_initialize(void);
 void opcontrol(void);
-
 #ifdef __cplusplus
 }
 #endif
 
 #ifdef __cplusplus
 /**
-
+ * You can add C++-only headers here
  */
 //#include <iostream>
-
 #endif
 
-#endif // _PROS_MAIN_H_
+#endif  // _PROS_MAIN_H_
